e5f20e22f88a74c03028a294a993a1a56d362196
mijal.shrestha <mijalshrestha88@gmail.com>
2024-05-08 16:44:45 -0400

    Fixed unique contraint

--- backend/job_model.py
+++ backend/job_model.py
@@ -2,11 +2,11 @@ from config import db
 
 class Job(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    company_name = db.Column(db.String(800), unique =False, nullable=False)
-    position = db.Column(db.String(800), unique =False, nullable=False)
-    job_link = db.Column(db.String(800), unique =True, nullable=False)
-    date_applied = db.Column(db.String(100), unique =True, nullable=False)
-    additional_info = db.Column(db.String(800), unique =True, nullable=False)
+    company_name = db.Column(db.String(800), unique=False, nullable=False)
+    position = db.Column(db.String(800), unique=False, nullable=False)
+    job_link = db.Column(db.String(800), unique=False, nullable=False)
+    date_applied = db.Column(db.String(800), unique=False, nullable=False)
+    additional_info = db.Column(db.String(800), unique=False, nullable=False)
 
     def to_json(self):
         return {
@@ -15,6 +15,5 @@ class Job(db.Model):
             "position": self.position,
             "jobLink": self.job_link,
             "dateApplied": self.date_applied,
-            "additionalInfo" : self.additional_info
-
+            "additionalInfo": self.additional_info,
         }
